1179f48057668e76da3169b61c0bb4da1c7506e7
matthewlukyanov2 <brucelukyanov@gmail.com>
2024-12-24 16:38:51 +0000

    Set up MySQL connection in mysqlDao.js

--- models/mysqlDao.js
+++ models/mysqlDao.js
@@ -1,1 +1,13 @@
-ECHO is on.
+const pmysql = require('promise-mysql');
+
+// Create MySQL pool
+let pool;
+
+pmysql.createPool({
+    connectionLimit: 3,
+    host: 'localhost',
+    user: 'root', //MySQL user
+    password: 'Cooldude123!', //MySQL password
+    database: 'proj2024mysql', //Database
+})
+
