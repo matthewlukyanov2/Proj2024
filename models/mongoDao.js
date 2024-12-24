08baf427614719b83c69b38268a363911c092ead
matthewlukyanov2 <brucelukyanov@gmail.com>
2024-12-24 18:09:28 +0000

    added mongoDB connection to mongoDao.js

--- models/mongoDao.js
+++ models/mongoDao.js
@@ -1,1 +1,8 @@
-ECHO is on.
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/proj2024MongoDB', { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('MongoDB connected'))
+.catch((error) => console.log('MongoDB connection error:', error));
+
+
