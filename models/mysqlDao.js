94fb6b1cca8a07a552a07e85bbb518490566771c
matthewlukyanov2 <brucelukyanov@gmail.com>
2024-12-24 16:44:12 +0000

    Set up MySQL pool connection and added getStudents function in mysqlDao.js for fetching student data

--- models/mysqlDao.js
+++ models/mysqlDao.js
@@ -10,4 +10,25 @@ pmysql.createPool({
     password: 'Cooldude123!', //MySQL password
     database: 'proj2024mysql', //Database
 })
+.then((p) => {
+    pool = p;
+})
+.catch((e) => {
+    console.log("Pool error: " + e);
+});
 
+// Function to fetch students from the MySQL database
+var getStudents = function() {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * FROM student ORDER BY sid')
+        .then((data) => {
+            resolve(data);
+        })
+        .catch((error) => {
+            reject(error);
+        });
+    });
+};
+
+//CRUD operations
+module.exports = { getStudents };
