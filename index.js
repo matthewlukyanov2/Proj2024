322e7596e06e40340aa56643b20b79f6a7a2fe8f
matthewlukyanov2 <brucelukyanov@gmail.com>
2024-12-25 16:42:56 +0000

    removed unecessary app.js class and app listen to start the server in index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var express = require('express');
 var mysqlDAO = require('./models/mysqlDao'); // MySQL functions
 var mongoDAO = require('./models/mongoDao'); // MongoDB functions
+
+var app = express();
+
+// Start the server
+app.listen(3004, () => {
+    console.log("Running on port 3004");
+});
